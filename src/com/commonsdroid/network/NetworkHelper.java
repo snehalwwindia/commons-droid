a52d393cf7550d2e63f2488e6b0e564a926085b8
Melbourne Lopes <melbourne.lopes@wwindia.com>
2013-10-17 22:04:29 +0530

    getNetworkIpAddress() implemented

--- src/com/commonsdroid/network/NetworkHelper.java
+++ src/com/commonsdroid/network/NetworkHelper.java
@@ -4,6 +4,13 @@
  * */
 package com.commonsdroid.network;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.http.conn.util.InetAddressUtils;
+
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -21,7 +28,7 @@ public class NetworkHelper {
 	}
 
 	public static String getLocalIpAddress(Context context) {
-		/** This method returns the IP address of networked device */
+		/** This method returns the Local IP address */
 		WifiManager wifiMgr = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
 		WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
 		int ip = wifiInfo.getIpAddress();
@@ -29,4 +36,30 @@ public class NetworkHelper {
 
 		return address;
 	}
+	
+	public static String getNetworkIPAddress(boolean useIPv4) {
+		/** This method returns the Network IP address */
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        String sAddr = addr.getHostAddress().toUpperCase();
+                        boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr); 
+                        if (useIPv4) {
+                            if (isIPv4) 
+                                return sAddr;
+                        } else {
+                            if (!isIPv4) {
+                                int delim = sAddr.indexOf('%'); // drop ip6 port suffix
+                                return delim<0 ? sAddr : sAddr.substring(0, delim);
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex) { } // for now eat exceptions
+        return "";
+    }
 }
