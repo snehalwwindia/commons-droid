7a6c1c55186db275c71ebe88f764fc56d4a065ff
Melbourne Lopes <melbourne.lopes@wwindia.com>
2013-10-17 18:19:58 +0530

    ip address implemented

--- src/com/commonsdroid/network/NetworkHelper.java
+++ src/com/commonsdroid/network/NetworkHelper.java
@@ -1,15 +1,32 @@
+/**This class contains Network Utilities.
+ * 
+ * @author Melbourne Lopes.
+ * */
 package com.commonsdroid.network;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.text.format.Formatter;
 
 public class NetworkHelper {
+
 	public static boolean isNetworkAvailable(Context ctx) {
-		ConnectivityManager connectivityManager = (ConnectivityManager) ctx
-				.getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo activeNetworkInfo = connectivityManager
-				.getActiveNetworkInfo();
+		/** This method returns the network state */
+		ConnectivityManager connectivityManager = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
 		return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+	}
+
+	public static String getLocalIpAddress(Context context) {
+		/** This method returns the IP address of networked device */
+		WifiManager wifiMgr = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+		WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
+		int ip = wifiInfo.getIpAddress();
+		String address = Formatter.formatIpAddress(ip);
+
+		return address;
 	}
 }
